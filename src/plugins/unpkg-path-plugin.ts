94e4c80b48618379cf93125a49bf57d9d5763134
Roman Tarnakin <roman_tarnakin@epam.com>
2023-02-06 17:15:45 +0300

    add caching layer for unpkg plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,12 @@
 import type * as esbuild from 'esbuild-wasm'
 import axios from 'axios'
+import * as localforage from 'localforage'
+
+// We create a "localforage" instance which is an abstraction over IndexedDB
+// (which is built in the browser), that helps us use it in a comfortable way:
+const fileCache = localforage.createInstance({
+  name: 'fileCache'
+})
 
 // This is a CUSTOM PLUGIN for ESBUILD.
 // Why do we need it?
@@ -79,10 +86,22 @@ export const unpkgPathPlugin = (): esbuild.Plugin => {
           }
         }
 
-        // Here we are making a request to UNPKG service to try and load package contents
-        const { data, request } = await axios.get(args.path)
+        // We need to check if we already fetched the package and if it is in the cache.
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        )
 
-        return {
+        if (cachedResult != null) {
+          // If so then we return it:
+          return cachedResult
+        }
+        // If there's nothing in the cache then we make a request
+        // to UNPKG service to try and load package contents:
+        const { data, request } = await axios.get(args.path)
+        // If we successfully got the response from the UNPKG service,
+        // then we need to:
+        // 1) Construct an object compatible with ESBUILD:
+        const requestResult: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           // Here we construct a correct module resolution path. Why do we need to do it?
@@ -93,6 +112,10 @@ export const unpkgPathPlugin = (): esbuild.Plugin => {
           // endpoint, we need to use the real endpoint value(the latter one).
           resolveDir: new URL('./', request.responseURL).pathname
         }
+        // 2) Store the object in the cache:
+        await fileCache.setItem(args.path, requestResult)
+
+        return requestResult
       })
     }
   }
