3b1d70dc584511136c17bccb6473f3abb0f4e01d
Roman Tarnakin <roman_tarnakin@epam.com>
2023-02-08 18:39:09 +0300

    start adding text-editor

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
+import React, { useEffect, useRef, useState } from 'react'
+import MDEditor from '@uiw/react-md-editor'
+
+const TextEditor: React.FC = () => {
+  // This piece of state is for tracking the current mode
+  // and using it to render corresponding component.
+  const [isEditMode, setIsEditMode] = useState(false)
+  // We need this ref for tracking the on DIV click event.
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const listener = (evt: MouseEvent): void => {
+      // When click event bubbles up, we want to check its target(source) property.
+      // If the click was inside the div that is the container for text-editor,
+      // then we don't want to exit edit-mode.
+      if (
+        ref.current != null &&
+        evt.target != null &&
+        // Here we check whether a node is a descendant of a given node,
+        // i.e. the node itself, one of its direct children (childNodes),
+        // one of the children's direct children, and so on.
+        ref.current.contains(evt.target as Node)
+      ) {
+        return
+      }
+
+      // We exit edit-mode if the click was outside the text-editor
+      setIsEditMode(false)
+    }
+
+    document.addEventListener('click', listener, { capture: true })
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true })
+    }
+  }, [])
+
+  const onMarkdownClick = (): void => {
+    setIsEditMode(true)
+  }
+
+  if (isEditMode) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    )
+  }
+  return (
+    // We have to use "onClick" event handler on DIV
+    // just because the component does not have it...
+    <div onClick={onMarkdownClick}>
+      <MDEditor.Markdown source={'# Header'} />
+    </div>
+  )
+}
+
+export default TextEditor
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import CodeCell from './components/code-cell'
+// import CodeCell from './components/code-cell'
+import TextEditor from './components/text-editor'
 
 const App: React.FC = () => {
-  return <CodeCell />
+  // return <CodeCell />
+  return (
+    <div>
+      <TextEditor />
+    </div>
+  )
 }
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
