494b3082e097906cabf5b3b1574fdbbaa8fe3a83
Roman Tarnakin <roman_tarnakin@epam.com>
2023-02-09 10:44:56 +0300

    start adding action types

---
+++ src/store/action-types/index.ts
@@ -0,0 +1,7 @@
+// We define all the possible Action types (descriptions) that we will use
+export enum ActionType {
+  MOVE_CELL = 'move_cell',
+  DELETE_CELL = 'delete_cell',
+  INSERT_CELL_BEFORE = 'insert_cell_before',
+  UPDATE_CELL = 'update_cell',
+}
