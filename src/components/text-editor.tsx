f1e7dd87acf8612937862e58454c1440d82bafba
Roman Tarnakin <roman_tarnakin@epam.com>
2023-02-09 10:07:50 +0300

    add state to text-editor to show user input in preview version of component

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -4,11 +4,14 @@ import MDEditor from '@uiw/react-md-editor'
 import './text-editor.css'
 
 const TextEditor: React.FC = () => {
+  // We need this ref for tracking the on DIV click event.
+  const ref = useRef<HTMLDivElement>(null)
   // This piece of state is for tracking the current mode
   // and using it to render corresponding component.
   const [isEditMode, setIsEditMode] = useState(false)
-  // We need this ref for tracking the on DIV click event.
-  const ref = useRef<HTMLDivElement>(null)
+  // Thi piece of state is for storing the user input that was made inside the editor
+  // so that we can show it also in preview version of our component
+  const [editorInput, setEditorInput] = useState('# Header')
 
   useEffect(() => {
     const listener = (evt: MouseEvent): void => {
@@ -41,19 +44,27 @@ const TextEditor: React.FC = () => {
     setIsEditMode(true)
   }
 
+  const onEditorChange = (input: string | undefined): void => {
+    // We have to use ternary operator here because input can be either "string"
+    // or "undefined" and our piece of state expects "string" only
+    setEditorInput(input != null ? input : '')
+  }
+
   if (isEditMode) {
     return (
       // We are adding the className to be able to override default styling
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={editorInput} onChange={onEditorChange} />
       </div>
     )
   }
   return (
     // We have to use "onClick" event handler on DIV
     // just because the component does not have it...
-    <div className="text-editor" onClick={onMarkdownClick}>
-      <MDEditor.Markdown source={'# Header'} />
+    <div className="text-editor card" onClick={onMarkdownClick}>
+      <div className="card-content">
+        <MDEditor.Markdown source={editorInput} />
+      </div>
     </div>
   )
 }
