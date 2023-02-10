5c70d89c9250f181696c7abc55d1732cc6a4b4fa
Roman Tarnakin <roman_tarnakin@epam.com>
2023-02-10 17:23:11 +0300

    refactor CodeCell, Preview components

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -6,6 +6,37 @@ interface PreviewProps {
   bundlingError: string
 }
 
+// Now when ESBUILD has bundled and transpiled code, there is another major question:
+// how are we going to execute the code user provided?
+// The straightforward way of doing this is using EVAL:
+// eval(result.outputFiles[0].text)
+// BUT:
+// 1) User-provided code might throw errors and cause app to crash.
+// 2) User-provided code might mutate the DOM, causing our app to crash.
+// 3) User might accidentally run code provided by malicious user etc.
+// So it looks like using EVAL like this is not what we want, and we have to
+// do some preparations first before we can really use it safely.
+// To solve this problem we can use IFRAME with correct settings("sandbox"
+// and/or "src" attributes) AND/OR serve HTML for the iframe from different domain/port
+// to disallow cross-frame access.
+// How will it address the issues mentioned before?
+// Well:
+// 1) JS error inside an iframe WILL NOT crash the app
+// because JS code inside an iframe has different execution context.
+// 2) DOM mutations inside an iframe can be applied ONLY to the "document"
+// that is inside this iframe (iframe must be correctly set up).
+// 3) Malicious code (input onChange event listeners for stealing creds,
+// accessing document.cookie etc.) can be applied ONLY to the "document"
+// that is inside this iframe (iframe must be correctly set up).
+
+// We've decided NOT to go with SERVING HTML for an IFRAME at the different domain/port for the
+// sake of (1)simplicity (we don't have to set up additional server infrastructure)
+// AND (2)performance (we don't have to make any requests to fetch some HTML for rendering it inside iframe).
+// What are the consequences of this choice?
+// User will have some LIMITATIONS to the code that they can write and execute inside our app:
+// 1) No localStorage access.
+// 2) No cookies access.
+
 // This HTML is rendered inside an iframe via "srcDoc" attribute.
 // We are referencing "window" object of the iframe (every iframe has its own
 // window and document objects) and listening for a "message" event that we post/send
